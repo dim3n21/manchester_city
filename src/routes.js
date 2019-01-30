fd34989169d4a395fe93da728a05993c02b49014
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-01-29 16:11:15 -0800

    Set up project. Build Header and Footer.

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from './Hoc/Layout';
+
+
+const Routes = () => {
+  return (
+    <Layout>
+      second Line
+    </Layout>
+  )
+}
+
+export default Routes;
