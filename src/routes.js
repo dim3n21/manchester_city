1eedca5c7314d6895909d80bbf742e69b01dbe61
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-01-30 16:25:26 -0800

    Build Home featured animation

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Layout from './Hoc/Layout';
+import { Switch, Route } from 'react-router-dom';
 
+import Home from './Components/home';
 
-const Routes = () => {
+const Routes = (props) => {
   return (
     <Layout>
-      second Line
+        <Switch>
+            <Route exact component={Home} path="/" />
+        </Switch>
     </Layout>
   )
 }
