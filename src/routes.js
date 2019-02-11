e2c9eedee36a5feb400ac616e4571419ebe832de
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-02-11 11:34:40 -0800

    Build the Dashboard component. Build the Nav. Add firebase  authentication sign out button and logic.

--- src/routes.js
+++ src/routes.js
@@ -5,10 +5,13 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Components/home';
 import Signin from './Components/signin'
 
+import Dashboard from './Components/admin/Dashboard';
+
 const Routes = (props) => {
   return (
     <Layout>
         <Switch>
+          <Route exact component={Dashboard} path="/dashboard" />
           <Route exact component={Signin} path="/signin" />
           <Route exact component={Home} path="/" />
 
