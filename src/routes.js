44ebc076e09719cc15409b127e5ab5616dedb96d
Dmitry Andrianov <dima.andrianov@gmail.com>
2019-02-11 15:33:35 -0800

    Implement Private Routes, to see dashboard only when authenticated

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,19 @@ import React from 'react';
 import Layout from './Hoc/Layout';
 import { Switch, Route } from 'react-router-dom';
 
+import PrivateRoutes from './Components/authroutes/privateRoutes';
+
 import Home from './Components/home';
 import Signin from './Components/signin'
 
 import Dashboard from './Components/admin/Dashboard';
 
 const Routes = (props) => {
+
   return (
     <Layout>
         <Switch>
-          <Route exact component={Dashboard} path="/dashboard" />
+          <PrivateRoutes {...props} path="/dashboard" exact component={Dashboard}/> 
           <Route exact component={Signin} path="/signin" />
           <Route exact component={Home} path="/" />
 
